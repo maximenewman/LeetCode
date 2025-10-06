6355f675bdba8808f81da523017f202953bb9193
Maxime <maximenewman05@gmail.com>
2025-10-06 00:53:14 -0700

    Time: 4 ms (41.37%) | Memory: 18.9 MB (33.63%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,26 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result = list()
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if(nums[i] + nums[j] == target):
-                    result.append(i)
-                    result.append(j)
-        return result
+        """
+        We want to find the target, and store the indices that sum to it
+        To do this, we will create a new array to store our indeces
+        Since the question, specifies that there is only 1 pair of indices
+        we can imediately return, once we find the matching indices
+        [3,4,5,6]
+        """
+
+        new_map: dict = {}
+        # high: int = len(nums) - 1
+        """
+        Since we are always incrementing
+        Then we are only searching for values that are already in the list
+        Hence we know that the new values index will always be greater than the old values
+        That we are checking for
+        """
+        for i in range(len(nums)):
+            if (target - nums[i]) in new_map:
+                return [new_map[target-nums[i]], i]
+            if nums[i] not in new_map:
+                new_map[nums[i]] = i
+
+        print(new_map)
+        return []
